74e3537dd07416e8edbeecea406d8d29481d1516
SPC <SPC@20-LAB402>
2024-08-05 18:58:50 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+namespace clock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString("hh:mm:ss");
+        }
+    }
+}
